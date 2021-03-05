68de46174a3e29124505b89275372db50e45095f
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-03-05 15:12:22 +0800

    Fusion des fichiers js produit et accueil pour un seul fichier commun aux deux pages

--- javascript/Accueil.js
+++ javascript/Accueil.js
@@ -1,4 +1,4 @@
-/* == AJOUT DU BLOC HTML RECEVANT LE CONTENU == */
+/* == BLOC HTML PAGE ACCUEIL == */
 
 var creationBlocHtmlAccueil = function (nom, description, prix, id, image) { /* Parametre correspondant aux donnees API de chaque element */
 
@@ -43,7 +43,7 @@ var creationBlocHtmlAccueil = function (nom, description, prix, id, image) { /* 
     prixProduit.innerHTML = "Prix: " + prix + " €"
 }
 
-/* == ENVOIE DES DONNES SUR LA PAGE ACCUEIL == */
+/* == ENVOIE DES DONNEES SUR LA PAGE ACCUEIL == */
 
 const listeProduitsAccueil = function(tableau) {
     
@@ -51,6 +51,96 @@ const listeProduitsAccueil = function(tableau) {
         creationBlocHtmlAccueil(tableau[i].name, tableau[i].description, 
         tableau[i].price, tableau[i]._id, tableau[i].imageUrl);
        
+    }
+}
+
+/* == BLOC HTML PAGE PRODUIT == */
+
+var creationBlocHtmlProduit = function (nom, description, prix, image) {
+
+    const article = document.getElementById("article")
+
+    const nomProduit = document.createElement("h3")
+    nomProduit.classList.add("text-secondary", "col-12", "p-3")
+
+    const elementProduit = document.createElement("div")
+    elementProduit.classList.add("col-md-5", "mb-3")
+
+    const descriptionProduit = document.createElement("p")
+    descriptionProduit.classList.add("text-secondary", "pt-5")
+
+    const prixProduit = document.createElement("p")
+    prixProduit.setAttribute("id", "prix")
+    prixProduit.classList.add("text-secondary", "pt-5", "font-weight-bold")
+
+    const imageProduit = document.createElement("img") 
+    imageProduit.setAttribute("src", image)
+    imageProduit.classList.add("col-md-7", "mb-3", "rounded")
+
+    article.appendChild(nomProduit)
+    
+
+    article.appendChild(elementProduit)
+    elementProduit.appendChild(descriptionProduit)
+    elementProduit.appendChild(prixProduit)
+
+    article.appendChild(imageProduit)
+
+    nomProduit.innerHTML = "Ourson : " + nom
+    nomProduit.style.fontStyle = "italic"
+
+    descriptionProduit.innerHTML = "Description : " + description
+    descriptionProduit.style.fontSize = "1.3rem"
+    
+    prixProduit.innerHTML = "Prix : " + prix + " €"
+    prixProduit.style.fontSize = "1.4rem"
+}
+
+/* == ENVOIE DES DONNEES SUR LA PAGE PRODUIT == */
+
+const descriptifProduit = function(donnees) {
+    
+    creationBlocHtmlProduit(donnees.name, donnees.description, 
+        donnees.price, donnees.imageUrl)
+
+}
+
+/* == LES FONCTION POUR LA PERSONNALISATION DU PRODUIT ET SON MENU DEROULANT == */
+
+var nouvelOngletPersonnalisation = function (element) {
+
+    var ongletMenu = document.querySelector("#navbarContent ul")
+
+    var itemMenu = document.createElement("li")
+    itemMenu.classList.add("nav-item", "border-bottom", "border-dark")
+
+    var contenuItem = document.createElement("a")
+    contenuItem.classList.add("lien_nav", "nav-link", "text-primary", "font-weight-bold", "m-2")
+    contenuItem.setAttribute("href", "#")
+
+    ongletMenu.appendChild(itemMenu)
+    itemMenu.appendChild(contenuItem)
+
+    contenuItem.innerHTML = element
+}
+
+var menuPresonnalisation = function (donnees) {
+
+    var boutonPersonnalisation = document.getElementById("btn_personnalisation")
+    var premierItemMenu = document.querySelector("#navbarContent ul li a")
+
+    if (donnees == undefined || donnees.length == 0) { /* Si il n'y a pas de données ou que le tableau n'existe pas */
+        boutonPersonnalisation.setAttribute("disabled", "")
+        throw ("les donnees ou le tableau n'existent pas")
+
+    } else {
+
+        premierItemMenu.innerHTML= donnees[0] /* Renvoie a la 1ere données pour conserver la class active (bootstrap) des items du menu */
+
+        for (i=1; i < donnees.length; i++) { /* commence au 2eme élement du tableau car la 1ere est deja incrementer */
+            
+            nouvelOngletPersonnalisation (donnees[i])
+        }
     }
 }
 

a98f9cd6069bd289fd71c674cc414c1f43d751c6
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-03-06 18:25:21 +0800

    Fonction listeProduitsPanier finalisé et integration des fonctions compteur panier et suppression panier

--- javascript/panier test.js
+++ javascript/panier test.js
@@ -5,13 +5,12 @@ var creationBlocHtmlPanier = function (image, nom, prix, quantite) { /* Parametr
     const produitPanier = document.getElementById("produit_Panier")
 
     const produit = document.createElement("article")
-    produit.classList.add("p-4", "row")
+    produit.classList.add("p-4", "row", "bg-light")
 
     const blocimage = document.createElement("div")
     blocimage.classList.add ("col-3")
 
     const imageProduit = document.createElement("img")
-    imageProduit.classList.add("col-3")
     imageProduit.setAttribute("src", image) /* AJOUT DU PARAMETRE IMAGE QUI RECUPERE L'URL DE L'IMAGE CORRESPONDANTE */
     imageProduit.setAttribute("alt", "image de l'ourson " + nom) /* AJOUT DU NOM DE L'OURSON */
 
@@ -61,7 +60,80 @@ var creationBlocHtmlPanier = function (image, nom, prix, quantite) { /* Parametr
 
 }
 
+/* PANIER VIDE */
 
+var panierVide = function() {
+
+    const produitPanier = document.getElementById("produit_Panier")
+
+    const produit = document.createElement("article")
+    produit.classList.add("p-4", "row", "bg-light")
+
+    const ContentPanierVide = document.createElement("p")
+
+    produitPanier.appendChild(produit)
+    produit.appendChild(ContentPanierVide)
+
+    ContentPanierVide.innerHTML = "Votre panier est vide"
+}
+
+/* NOMBRE PRODUIT PANIER */
+
+var nombreProduitsPanier = function() {
+    
+    var quantiteTotale = document.querySelector(".recapitulatif .quantite")
+    var compteur = 0
+    for (i=0; i < localStorage.length; i++) {
+        
+        var nombreClick = parseInt(localStorage.getItem(localStorage.key(i)))
+        compteur += nombreClick
+    }
+
+    if (compteur > 0) {
+        quantiteTotale.innerHTML = compteur
+    } else {
+        quantiteTotale.innerHTML = "0"
+    }
+}
+
+/* VIDER LE PANIER */
+
+var viderPanier = function() {
+    
+    var boutonViderPanier = document.querySelector("#btn_paniervide")
+    boutonViderPanier.addEventListener('click', function() {
+        localStorage.clear()
+        document.location.reload();
+    })
+
+}
+
+/* == LISTE PRODUITS PAGE PANIER == */
+
+var ListeProduitsPanier = function(donnees) {
+   
+    if (localStorage.length === 0) {
+
+        panierVide()
+        
+    }
+
+    var idEnCache = Object.keys(localStorage)   /* Je charge le localstorage dans 1 tableau regroupant les cles */
+    var valeurEnCache = Object.values(localStorage) /* puis dans un tableau regroupant les valeurs. L'index permet d'associer l'id a sa valeur */
+
+    for (i=0; i < donnees.length; i++) {
+
+        var IdDonneesAPI = donnees[i]._id /* recuperer l'id de l'API a chaque boucle */
+
+        if (idEnCache.indexOf(IdDonneesAPI) != -1) { /* Si l'id correspond a un index du tableau regroupant les cles, alors je fais la fonction suivante */
+            
+            var getIndexValeurCache = valeurEnCache[idEnCache.indexOf(IdDonneesAPI)]
+            var getIndexDonneesAPI = i
+            creationBlocHtmlPanier(donnees[getIndexDonneesAPI].imageUrl, donnees[getIndexDonneesAPI].name,
+             donnees[getIndexDonneesAPI].price, getIndexValeurCache)
+        }        
+    }
+} 
 
 /* == RECUPERATION DES DONNEES API == */
 
@@ -83,36 +155,9 @@ var recuperationDonneesApi = async function () {
 
                 .then (function(donnees) {
                     
-
-for (i=0; i < localStorage.length;i++){
-    
-    var donneesCache = localStorage.key(i)
-    console.log(donneesCache)
-
-    for (i=0; i < donnees.length;i++) {
-        var donneesAPI = donnees[i]._id
-        console.log(donneesAPI)
-
-        if (donneesCache === donneesAPI) {
-            var result = 'true'
-            var indexDonneesPresente = i 
-        }
-    }
-
-}
-
-
-console.log(indexDonneesPresente)
-
-
-
-
-
-
-
-
-
-
+                    nombreProduitsPanier()
+                    viderPanier()
+                    ListeProduitsPanier(donnees)
 
                 })
 
